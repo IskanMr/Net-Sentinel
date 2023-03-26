21710d807932e5ee26ca196dba871bf6d0ca2cd9
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2023-03-26 23:18:21 +0700

    feat(frontEnd): added home (not monitoring)

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,19 @@
 import DefaultLayout from "../layouts/DefaultLayout";
 
+import Button from "../components/utils/Button";
+
+import Styles from "../styles/pages/index.module.css";
+
 export default function Home() {
-  return <DefaultLayout seoTitle="Home">Home</DefaultLayout>;
+  return (
+    <DefaultLayout seoTitle="Home">
+      <div className={Styles.container}>
+        <div className={Styles.content}>
+          <img src="/assets/notMonitor.svg" alt="Not Monitoring" />
+          <h1 style={{ textAlign: "center" }}>Not Monitoring</h1>
+        </div>
+        <Button>Start Monitoring</Button>
+      </div>
+    </DefaultLayout>
+  );
 }
