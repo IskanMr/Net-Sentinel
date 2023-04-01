d290673813d17ceb286a1c11d347517d1478c7cd
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2023-04-02 00:18:56 +0700

    feat(home): added state function

--- frontend/src/components/utils/Button.js
+++ frontend/src/components/utils/Button.js
@@ -1,11 +1,12 @@
 import Styles from "./Button.module.css";
 
-export default function Button({ children, onClick, type, disabled }) {
+export default function Button({ children, onClick, type, disabled, style }) {
   return (
     <button
       className={`${Styles.button} ${Styles[type]}`}
       onClick={onClick}
       disabled={disabled}
+      style={style}
     >
       {children}
     </button>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import DefaultLayout from "../layouts/DefaultLayout";
 
 import Button from "../components/utils/Button";
@@ -5,14 +7,38 @@ import Button from "../components/utils/Button";
 import Styles from "../styles/pages/index.module.css";
 
 export default function Home() {
+  const [conState, setConState] = useState("No Monitoring!");
+  const [conStart, setConStart] = useState(false);
+
+  const start = () => {
+    !conStart ? setConState("Monitoring") : setConState("No Monitoring!");
+    setConStart(!conStart);
+  };
+
+  useEffect(() => {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+    if (!token) {
+      window.location.replace("/login");
+    }
+  });
+
   return (
     <DefaultLayout seoTitle="Home">
       <div className={Styles.container}>
         <div className={Styles.content}>
-          <img src="/assets/notMonitor.svg" alt="Not Monitoring" />
-          <h1 style={{ textAlign: "center" }}>Not Monitoring</h1>
+          <img
+            src={`/assets/${
+              conState == "Monitoring" ? "save.svg" : "notMonitor.svg"
+            }`}
+            alt="Not Monitoring"
+          />
+          <h1 style={{ textAlign: "center" }}> {conState}</h1>
         </div>
-        <Button>Start Monitoring</Button>
+        <Button type={conStart ? "active" : ""} onClick={start}>
+          {conStart ? "Stop monitoring" : "Start monitoring"}
+        </Button>
       </div>
     </DefaultLayout>
   );
