38a98080003c5aa31611552b8c44f3a23939b70c
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2023-06-05 00:06:49 +0700

    fix

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ users = db.users
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://net-sentinel.vercel.app/", "http://localhost:3000"],
+    allow_origins=["https://net-sentinel.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
